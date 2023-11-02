05f50c0d2f77453670a786dc47119b86f8e8c91d
kaelynj <kaelyn.ferris@gmail.com>
2023-11-02 00:22:34 -0400

    Fix prod url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   trailingSlash: 'true',
 
   // Set the production url of your site here
-  url: 'https://kaelynj.github.io/sanctuary-amongst-the-stars/',
+  url: 'https://kaelynj.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/sanctuary-amongst-the-stars/',
